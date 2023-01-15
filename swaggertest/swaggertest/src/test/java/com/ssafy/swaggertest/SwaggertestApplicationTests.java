a6e13c19b549c74e7e93ea8bf948d5056067fd81
hjs101 <gkswlstjdwkd@naver.com>
2023-01-15 21:32:37 +0900

    first commit

---
+++ swaggertest/swaggertest/src/test/java/com/ssafy/swaggertest/SwaggertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.swaggertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
