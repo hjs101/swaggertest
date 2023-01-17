2527a27a84ffc51b46443a8c38f079cfd47b3504
hjs101 <gkswlstjdwkd@naver.com>
2023-01-18 07:33:27 +0900

    1234

--- crudtest-1/src/main/java/com/ssafy/crudtest/entity/Content.java
+++ crudtest-1/src/main/java/com/ssafy/crudtest/entity/Content.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,13 +18,18 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Schema(description = "게시글")
 public class Content {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Schema(description = "pk" , example = "1")
 	private int uid;
+	@Schema(description = "파일 경로" , example = "file.png")
 	private String path;
+	@Schema(description = "글 제목" , example = "title")
 	private String title;
+	@Schema(description = "비밀번호" , example = "password")
 	private String password;
 	
 	@Builder
